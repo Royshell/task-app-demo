9de1fdc65a5ba36ec6644959dd80762b320764f9
Roy Shell <roy.shell5@gmail.com>
2020-11-25 13:17:08 +0200

    Added funtions

--- src/App.js
+++ src/App.js
@@ -1,53 +1,55 @@
-import Column from './Components/Column';
+
+import Board from './Components/Board';
 import './App.scss';
-import firebase from 'firebase/app' ;
-import 'firebase/firestore';
-import 'firebase/functions';
+// import firebase from 'firebase/app' ;
+// import 'firebase/firestore';
+// import 'firebase/functions';
 
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCOV50yQlmst_cbtsADiVH3j-RcqrLrOL0",
-  authDomain: "roy-test-db73c.firebaseapp.com",
-  databaseURL: "https://roy-test-db73c.firebaseio.com",
-  projectId: "roy-test-db73c",
-  storageBucket: "roy-test-db73c.appspot.com",
-  messagingSenderId: "625353033482",
-  appId: "1:625353033482:web:54e3e50797b6929dd99cf3",
-  measurementId: "G-VP1C9GDGS0"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCOV50yQlmst_cbtsADiVH3j-RcqrLrOL0",
+//   authDomain: "roy-test-db73c.firebaseapp.com",
+//   databaseURL: "https://roy-test-db73c.firebaseio.com",
+//   projectId: "roy-test-db73c",
+//   storageBucket: "roy-test-db73c.appspot.com",
+//   messagingSenderId: "625353033482",
+//   appId: "1:625353033482:web:54e3e50797b6929dd99cf3",
+//   measurementId: "G-VP1C9GDGS0"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 // const trackTasks = firebase.functions().httpsCallable('trackTasks');
 // trackTasks().then(hi => console.log(hi));
 //const tickets = [{content: 'hi there'}, { content: 'hello world!'}];
-const candidateTickets = [];
-const qaTickets = [];
-const inProgressTickets = [];
-const completedTickets = []
-const ref = firebase.firestore().collection('boards').doc('Sales').collection('tasks');
+// const candidateTickets = [];
+// const qaTickets = [];
+// const inProgressTickets = [];
+// const completedTickets = []
+// const ref = firebase.firestore().collection('boards').doc('Sales').collection('tasks');
 
-ref.get().then(docs => {
-  console.log(docs);
-  docs.forEach(doc => {
-    console.log(doc.data());
-        switch(doc.data().status) {
-      case 'qa':
-        qaTickets.push(doc.data());
-        break;
-      case 'completed':
-        completedTickets.push(doc.data());
-        break;
-      case 'inProgress':
-        inProgressTickets.push(doc.data());
-        break;
-      case 'candidate':
-        candidateTickets.push(doc.data());  
-        break;    
-    } 
-  });
-});  
-console.log(qaTickets);
+// ref.get().then(docs => {
+//   console.log(docs);
+//   docs.forEach(doc => {
+//     console.log(doc.data());
+//         switch(doc.data().status) {
+//       case 'qa':
+//         qaTickets.push(doc.data());
+//         break;
+//       case 'completed':
+//         completedTickets.push(doc.data());
+//         break;
+//       case 'inProgress':
+//         inProgressTickets.push(doc.data());
+//         break;
+//       case 'candidate':
+//         candidateTickets.push(doc.data());  
+//         break;    
+//     } 
+//   });
+// });  
+//console.log(qaTickets);
 
 // ref.onSnapshot(snapshot => {
 //   // candidateTickets = snapshot.filter( doc => doc.status === 'candidate');
@@ -75,19 +77,28 @@ console.log(qaTickets);
 //     // } 
 //   });
 // });
+
+import firebase from 'firebase/app' ;
+import 'firebase/firestore';
+import 'firebase/functions';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOV50yQlmst_cbtsADiVH3j-RcqrLrOL0",
+  authDomain: "roy-test-db73c.firebaseapp.com",
+  databaseURL: "https://roy-test-db73c.firebaseio.com",
+  projectId: "roy-test-db73c",
+  storageBucket: "roy-test-db73c.appspot.com",
+  messagingSenderId: "625353033482",
+  appId: "1:625353033482:web:54e3e50797b6929dd99cf3",
+  measurementId: "G-VP1C9GDGS0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
 
   return (
-    <div className="board">
-      <Column title="Candidates" tickets={candidateTickets}>
-      </Column>
-      <Column title="In Progress" tickets={inProgressTickets}>
-      </Column>
-      <Column  title="QA/Code review" tickets={qaTickets}>
-      </Column>
-      <Column  title="Completed" tickets={completedTickets}>
-      </Column>
-    </div>
+    <Board/>
   );
 }
 
